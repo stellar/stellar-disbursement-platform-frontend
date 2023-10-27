be5dec7bc0e1706032816a102ea1a8c1313f84d8
Iveta <quietbits@gmail.com>
2023-10-27 13:57:44 -0400

    State refactor: reset password

---
+++ src/apiQueries/useNewPassword.ts
@@ -0,0 +1,49 @@
+import { useMutation } from "@tanstack/react-query";
+import { API_URL } from "constants/settings";
+import { fetchApi } from "helpers/fetchApi";
+import { sanitizeObject } from "helpers/sanitizeObject";
+import { AppError } from "types";
+
+type NewPasswordFields = {
+  currentPassword?: string;
+  newPassword?: string;
+};
+
+export const useNewPassword = () => {
+  const mutation = useMutation({
+    mutationFn: ({ currentPassword, newPassword }: NewPasswordFields) => {
+      const fieldsToSubmit = sanitizeObject({
+        current_password: currentPassword,
+        new_password: newPassword,
+      });
+
+      if (Object.keys(fieldsToSubmit).length < 2) {
+        throw Error(
+          "Update profile password requires current password and new password.",
+        );
+      }
+
+      return fetchApi(`${API_URL}/profile/reset-password`, {
+        method: "PATCH",
+        body: JSON.stringify(fieldsToSubmit),
+      });
+    },
+    cacheTime: 0,
+  });
+
+  return {
+    ...mutation,
+    error: mutation.error as AppError,
+    data: mutation.data as { message: string },
+    mutateAsync: async ({
+      currentPassword,
+      newPassword,
+    }: NewPasswordFields) => {
+      try {
+        await mutation.mutateAsync({ currentPassword, newPassword });
+      } catch (e) {
+        // do nothing
+      }
+    },
+  };
+};
