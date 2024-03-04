15c0177215d86bc06c46bac86b6b44af49e10b3e
Marwen Abid <marwen.abid@stellar.org>
2024-03-04 11:05:26 -0800

    SDP-1095 - [FE] getting 400 when calling GET `/organization/logo`, on branch `sdp-multitenant`

--- src/api/getOrgLogo.ts
+++ src/api/getOrgLogo.ts
@@ -1,7 +1,15 @@
 import { API_URL } from "constants/envVariables";
+import { getSdpTenantName } from "helpers/getSdpTenantName";
 
-export async function getOrgLogo(): Promise<string> {
-  const response = await fetch(`${API_URL}/organization/logo`);
+export async function getOrgLogo(token: string): Promise<string> {
+  const response = await fetch(`${API_URL}/organization/logo`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+      "SDP-Tenant-Name": getSdpTenantName(),
+    },
+  });
 
   const responseBlob = await response.blob();
   return URL.createObjectURL(responseBlob);
