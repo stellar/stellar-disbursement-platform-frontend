ea3261c07b8ab2a12c0a8b4212156b7b0b637943
Cecília Romão <ceciliaromao@gmail.com>
2024-04-05 12:37:37 -0300

    Update patchOrgInfo.ts

--- src/api/patchOrgInfo.ts
+++ src/api/patchOrgInfo.ts
@@ -17,28 +17,31 @@ export const patchOrgInfo = async (
   }
 
   const formData = new FormData();
+  let data = {};
 
   Object.entries(fieldsToSubmit).forEach(([key, value]) => {
     switch (key) {
       case "name":
-        formData.append("data", `{"organization_name": "${value}"}`);
+        data = { ...data, organization_name: `${value}` };
         break;
       case "privacyPolicyLink":
-        formData.append("data", `{"privacy_policy_link": "${value}"}`);
+        data = { ...data, privacy_policy_link: `${value}` };
         break;
       case "timezone":
-        formData.append("data", `{"timezone_utc_offset": "${value}"}`);
+        data = { ...data, timezone_utc_offset: `${value}` };
+        break;
+      case "isApprovalRequired":
+        data = { ...data, is_approval_required: value };
         break;
       case "logo":
         formData.append("logo", value as Blob);
-        break;
-      case "isApprovalRequired":
-        formData.append("data", `{"is_approval_required": ${value}}`);
         break;
       default:
         throw Error(`Update organization does not accept ${key} field`);
     }
   });
+
+  formData.append("data", `${JSON.stringify(data)}`);
 
   // BE always expects data object
   if (!formData.get("data")) {
