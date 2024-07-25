3cb270f363f8040f0107f58bcab876ab7e620b89
Iveta <quietbits@gmail.com>
2024-07-25 15:47:57 -0400

    Disable new disbursement btn for inactive circle account

--- src/components/NewDisbursementButton.tsx
+++ src/components/NewDisbursementButton.tsx
@@ -2,22 +2,35 @@ import { Button, Icon } from "@stellar/design-system";
 import { useNavigate } from "react-router-dom";
 import { ShowForRoles } from "components/ShowForRoles";
 import { Routes } from "constants/settings";
+import { useCircleAccount } from "hooks/useCircleAccount";
 
-export const NewDisbursementButton = () => {
+export const NewDisbursementButton = ({
+  size = "sm",
+}: {
+  size?: "sm" | "md" | "xs";
+}) => {
   const navigate = useNavigate();
+  const { isCircleAccount, isCircleAccountPending } = useCircleAccount();
 
   const goToNewDisbursement = () => {
     navigate(Routes.DISBURSEMENT_NEW);
   };
 
+  const inActiveCircleAccount = isCircleAccount && isCircleAccountPending;
+  const disableMessage = inActiveCircleAccount
+    ? "Circle Account is not active"
+    : "";
+
   return (
     <ShowForRoles acceptedRoles={["owner", "financial_controller"]}>
       <Button
         variant="primary"
-        size="sm"
+        size={size}
         icon={<Icon.Add />}
         iconPosition="right"
         onClick={goToNewDisbursement}
+        disabled={inActiveCircleAccount}
+        title={disableMessage}
       >
         New disbursement
       </Button>
