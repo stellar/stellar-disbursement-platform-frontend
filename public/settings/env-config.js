6996fb366038be9316692da4ad7e5c4052504330
Erica <erica.liu@stellar.org>
2024-08-15 12:22:36 -0700

    add env

---
+++ public/settings/env-config.js
@@ -0,0 +1,7 @@
+window._env_ = {
+  API_URL: "https://localhost:8000",
+  STELLAR_EXPERT_URL: "https://stellar.expert/explorer/testnet",
+  HORIZON_URL: "https://horizon-testnet.stellar.org",
+  RECAPTCHA_SITE_KEY: "6Lego1wmAAAAAJNwh6RoOrsHuWnsciCTIL3NN-bn",
+  SINGLE_TENANT_MODE: false,
+};
--- webpack.common.js
+++ webpack.common.js
@@ -155,6 +155,9 @@ module.exports = {
         REACT_APP_RECAPTCHA_SITE_KEY: JSON.stringify(
           process.env.REACT_APP_RECAPTCHA_SITE_KEY || "",
         ),
+        REACT_APP_SINGLE_TENANT_MODE: JSON.stringify(
+          process.env.REACT_APP_SINGLE_TENANT_MODE || "",
+        ),
         REACT_APP_USE_SSO: JSON.stringify(process.env.REACT_APP_USE_SSO || ""),
         REACT_APP_OIDC_AUTHORITY: JSON.stringify(
           process.env.REACT_APP_OIDC_AUTHORITY || "",
