stellar/stellar-disbursement-platform-frontend

commit 605e788c08ef2123534e30e4873ba9f992a767fe

[Fix] Do not allow selection of asset that does not have an account trustline / balance (#134)

src/components/DisbursementDetails/index.tsx
@@ -14,6 +14,7 @@ import { useVerificationTypes } from "apiQueries/useVerificationTypes";
 import { AssetAmount } from "components/AssetAmount";
 import { InfoTooltip } from "components/InfoTooltip";
 import { formatUploadedFileDisplayName } from "helpers/formatUploadedFileDisplayName";
+import { useAllBalances } from "hooks/useAllBalances";
 import {
   ApiAsset,
   ApiCountry,
@@ -96,6 +97,9 @@ export const DisbursementDetails: React.FC<DisbursementDetailsProps> = ({
     error: verificationTypesError,
     isFetching: isVerificationTypesFetching,
   } = useVerificationTypes();
+
+  // Get balances for distribution account
+  const { allBalances } = useAllBalances();
 
   const apiErrors = [
     countriesError?.message,
@@ -324,12 +328,24 @@ export const DisbursementDetails: React.FC<DisbursementDetailsProps> = ({
           value={details.asset.id}
           disabled={isWalletAssetsFetching || !details.wallet.id}
         >
-          {renderDropdownDefault(isWalletAssetsFetching)}
-          {walletAssets?.map((asset: ApiAsset) => (
-            <option key={asset.id} value={asset.id}>
-              {asset.code}
-            </option>
-          ))}
+          {walletAssets
+            ?.filter((wa: ApiAsset) => {
+              // Check for the default native asset
+              if (wa.code == "XLM") {
+                return wa;
+              }
+              // Check that the asset is non-native asset that has a distribution account balance
+              return allBalances?.find(
+                (balance) =>
+                  balance.assetCode === wa.code &&
+                  balance.assetIssuer === wa.issuer,
+              );
+            })
+            ?.map((wa: ApiAsset) => (
+              <option key={wa.id} value={wa.id}>
+                {wa.code}
+              </option>
+            ))}
         </Select>
 
         <Select

commit 33bbeaeeefa7ad25a6524fa758af1b1d8107bc91

[SDP-1323] Fix bug introduced in PR #134 where the upload csv button would never be enabled (#140)
### What

Fix bug introduced in PR #134 where the upload csv button would never be enabled.

### Why

The bug was caused because the PR removed the `{renderDropdownDefault(isWalletAssetsFetching)}` line of code that inserts the (initial) "empty dropdown value" to the dropdown list, and it resulted in preventing the _dropdown selection changed_ event from being called when the assets dropdown list contained a single item. That mistake prevents disbursement instructions from being uploaded.

### Additional Change(s)

Update the syntax of the javascript filter function to return explicit booleans.

---------

Co-authored-by: Iveta <quietbits@users.noreply.github.com>

src/components/DisbursementDetails/index.tsx
@@ -328,14 +328,15 @@ export const DisbursementDetails: React.FC<DisbursementDetailsProps> = ({
           value={details.asset.id}
           disabled={isWalletAssetsFetching || !details.wallet.id}
         >
+          {renderDropdownDefault(isWalletAssetsFetching)}
           {walletAssets
             ?.filter((wa: ApiAsset) => {
               // Check for the default native asset
-              if (wa.code == "XLM") {
-                return wa;
+              if (wa.code === "XLM" && wa.issuer === "") {
+                return true;
               }
-              // Check that the asset is non-native asset that has a distribution account balance
-              return allBalances?.find(
+              // Check that the asset is a non-native asset that has a distribution account balance
+              return !!allBalances?.find(
                 (balance) =>
                   balance.assetCode === wa.code &&
                   balance.assetIssuer === wa.issuer,