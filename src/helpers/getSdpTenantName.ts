e27c5a389f8c6774c5c0045bf528d9628b695e84
Iveta <quietbits@gmail.com>
2025-01-30 08:32:27 -0500

    Always check stored name

--- src/helpers/getSdpTenantName.ts
+++ src/helpers/getSdpTenantName.ts
@@ -2,9 +2,11 @@ import { DISABLE_TENANT_PREFIL_FROM_DOMAIN } from "constants/envVariables";
 import { localStorageTenantName } from "helpers/localStorageTenantName";
 
 export const getSdpTenantName = (organizationName?: string): string => {
+  const orgName = organizationName || localStorageTenantName.get();
+
   if (DISABLE_TENANT_PREFIL_FROM_DOMAIN === "true") {
-    return organizationName || localStorageTenantName.get() || "";
+    return orgName || "";
   }
 
-  return organizationName ?? window.location.hostname.split(".")[0];
+  return orgName || window.location.hostname.split(".")[0];
 };
