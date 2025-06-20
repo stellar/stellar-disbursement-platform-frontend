9c3f59fd45c41c41d2e07afed6fc45ce70f02c88
Denys Pohrebniak <dpohr@softserveinc.com>
2025-06-20 16:50:58 +0300

    SDP-1639 add empty message component

---
+++ src/components/EmptyStateMessage/EmptyStateMessage.tsx
@@ -0,0 +1,18 @@
+import { Icon } from "@stellar/design-system";
+
+import "./styles.scss";
+
+interface EmptyStateMessageProps {
+  icon?: React.ReactNode;
+  message: string;
+}
+
+export const EmptyStateMessage = ({
+  icon = <Icon.Key />,
+  message,
+}: EmptyStateMessageProps) => (
+  <div className="EmptyStateMessage">
+    {icon}
+    {message}
+  </div>
+);
