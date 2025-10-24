9b1dc7ba1a62899c893950fbc990ad6a7edb62a2
philipliu <philip.liu@stellar.org>
2025-10-24 14:59:04 -0400

    Move balance fetching into shared function

--- src/apiQueries/useWalletBalance.ts
+++ src/apiQueries/useWalletBalance.ts
@@ -1,11 +1,8 @@
-import { Asset, Networks } from "@stellar/stellar-sdk";
 import { useQuery } from "@tanstack/react-query";
-import BigNumber from "bignumber.js";
 
 import { createAuthenticatedRpcServer } from "@/helpers/createAuthenticatedRpcServer";
+import { fetchSACBalances } from "@/helpers/stellarBalances";
 import { AppError } from "@/types";
-
-const STROOP_CONVERSION_FACTOR = new BigNumber("10000000");
 
 interface WalletBalance {
   asset_code: string;
@@ -21,39 +18,18 @@ export const useWalletBalance = (contractAddress: string | undefined) => {
       }
 
       const rpcServer = createAuthenticatedRpcServer("wallet");
-      const passphrase = (await rpcServer.getNetwork())?.passphrase;
-      let network: Networks;
-      if (passphrase === Networks.PUBLIC) {
-        network = Networks.PUBLIC;
-      } else if (passphrase === Networks.FUTURENET) {
-        network = Networks.FUTURENET;
-      } else {
-        network = Networks.TESTNET;
-      }
 
-      // For now, we only support native XLM balance in embedded wallets
-      const asset = Asset.native();
+      const [walletBalance] =
+        (await fetchSACBalances({
+          rpcServer,
+          stellarAddress: contractAddress,
+          assets: [{ code: "XLM", issuer: null }],
+        })) ?? [];
 
-      try {
-        const balance = await rpcServer.getSACBalance(contractAddress, asset, network);
-
-        const balanceAmount = balance?.balanceEntry?.amount
-          ? new BigNumber(balance.balanceEntry.amount)
-              .dividedBy(STROOP_CONVERSION_FACTOR)
-              .toString(10)
-          : "0";
-
-        return {
-          asset_code: "XLM",
-          balance: balanceAmount,
-        };
-      } catch (error) {
-        // Return zero balance if fetching fails
-        return {
-          asset_code: "XLM",
-          balance: "0",
-        };
-      }
+      return {
+        asset_code: walletBalance?.asset_code ?? "XLM",
+        balance: walletBalance?.balance ?? "0",
+      };
     },
     enabled: Boolean(contractAddress),
     refetchInterval: 10000,
