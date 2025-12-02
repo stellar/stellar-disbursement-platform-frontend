141a8471eae3afe0261324a2f6cd7bb0e1b0d4de
Marwen Abid <marwen.abid@stellar.org>
2025-12-02 10:48:53 -0800

    SDP-1758: Added Wallet History for Stellar Distribution Accounts. (#385)
    ### What
    Added Wallet History for Stellar Distribution Accounts. 
    
    ### Why
    Allow an SDP owner / financial advisor to track transactions done
    through their distribution account.

--- src/components/WalletHistory.tsx
+++ src/components/WalletHistory.tsx
@@ -1,17 +1,17 @@
 import { Card, Link, Profile, Notification } from "@stellar/design-system";
-import { STELLAR_EXPERT_URL } from "@/constants/envVariables";
-import { useStellarAccountPayments } from "@/apiQueries/useStellarAccountPayments";
-import { formatDateTime } from "@/helpers/formatIntlDateTime";
 
-import { Table } from "@/components/Table";
+import { useStellarAccountPayments } from "@/apiQueries/useStellarAccountPayments";
 import { AssetAmount } from "@/components/AssetAmount";
 import { ErrorWithExtras } from "@/components/ErrorWithExtras";
+import { Table } from "@/components/Table";
+import { STELLAR_EXPERT_URL } from "@/constants/envVariables";
+import { formatDateTime } from "@/helpers/formatIntlDateTime";
 
-interface ReceiverWalletHistoryProps {
+interface WalletHistoryProps {
   stellarAddress: string | undefined;
 }
 
-export const ReceiverWalletHistory = ({ stellarAddress }: ReceiverWalletHistoryProps) => {
+export const WalletHistory = ({ stellarAddress }: WalletHistoryProps) => {
   const { isLoading, isFetching, data, error } = useStellarAccountPayments(stellarAddress);
 
   if (stellarAddress && (isLoading || isFetching)) {
@@ -65,7 +65,7 @@ export const ReceiverWalletHistory = ({ stellarAddress }: ReceiverWalletHistoryP
             ))
           ) : (
             <Table.BodyRow>
-              <td colSpan={5} className="ReceiverDetails__wallets__noRecentPayments">
+              <td colSpan={5} className="Table__emptyState">
                 No recent payments
               </td>
             </Table.BodyRow>
